12cc7dc2e8c1859108649d02bf3f4018823f9875
yoncho <joyonghyeon13@gmail.com>
2023-09-15 17:46:02 +0900

    study - mysite03

---
+++ mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -0,0 +1,150 @@
+package com.poscodx.mysite.repository;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.poscodx.mysite.vo.GuestbookVo;
+
+@Repository
+public class GuestbookRepository {
+	private final String URL = "jdbc:mariadb://192.168.0.181:3307/webdb?charset=utf8";
+	private final String ID = "mysite";
+	private final String PW = "mysite";
+	
+	public boolean insert(GuestbookVo vo) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		
+		try {
+			conn = getConnection();
+
+			String authorSql = "insert into guestbook values (null,?,?,?,now())";
+			pstmt = conn.prepareStatement(authorSql);
+			
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getContents());
+			
+			result =  pstmt.executeUpdate() == 1;
+		} catch(SQLException e) {
+			System.out.println("error:" + e);
+		} finally{
+			try {
+				//5. 자원 정리
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null && !conn.isClosed())
+				{
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public boolean delete(Long no, String password) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		
+		try {
+			conn = getConnection();
+
+			String deleteSql = "delete from guestbook where no=? and password=?";
+			pstmt = conn.prepareStatement(deleteSql);
+			
+			pstmt.setLong(1, no);
+			pstmt.setString(2, password);
+			
+			result =  pstmt.executeUpdate() == 1;
+		} catch(SQLException e) {
+			System.out.println("error:" + e);
+		} finally{
+			try {
+				//5. 자원 정리
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null && !conn.isClosed())
+				{
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+	
+	public List<GuestbookVo> findAll() {
+		List<GuestbookVo> result = new ArrayList<>();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = getConnection();
+			
+			String sql = "select no, name, contents, reg_date from guestbook order by reg_date desc";
+			pstmt = conn.prepareStatement(sql);
+			rs =  pstmt.executeQuery();
+
+			while (rs.next()) {
+				int no = rs.getInt(1);
+				String name = rs.getString(2);
+				String contents = rs.getString(3);
+				String regDate = rs.getString(4);
+				
+				GuestbookVo vo = new GuestbookVo();			
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setContents(contents);
+				vo.setRegDate(regDate);
+
+				result.add(vo);
+			}
+		} catch(SQLException e) {
+			System.out.println("error:" + e);
+		} finally{
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null && !conn.isClosed())
+				{
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;	
+	}
+	
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection(URL, ID, PW);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return conn;
+	}
+}
--- mysite03/src/main/webapp/WEB-INF/views/guestbook/main.jsp
+++ mysite03/src/main/webapp/WEB-INF/views/guestbook/main.jsp
@@ -17,9 +17,7 @@
 		<c:import url="/WEB-INF/views/includes/header.jsp" />
 		<div id="content">
 			<div id="guestbook">
-				<form action="${pageContext.request.contextPath}/guestbook"
-					method="post">
-					<input type="hidden" name="a" value="insert">
+				<form action="${pageContext.request.contextPath}/guestbook/add" method="post">
 					<table>
 						<tr>
 							<td>이름</td>
@@ -28,7 +26,7 @@
 							<td><input type="password" name="password"></td>
 						</tr>
 						<tr>
-							<td colspan=4><textarea name="content" id="content"></textarea></td>
+							<td colspan=4><textarea name="contents" id="content"></textarea></td>
 						</tr>
 						<tr>
 							<td colspan=4 align=right><input type="submit" VALUE=" 확인 "></td>
@@ -44,7 +42,7 @@
 									<td>[${count-status.index}]</td>
 									<td>${vo.name}</td>
 									<td>${vo.regDate}</td>
-									<td><a href="${pageContext.request.contextPath}/guestbook?a=deleteform&no=${vo.no}>">삭제</a></td>
+									<td><a href="${pageContext.request.contextPath}/guestbook/delete/${vo.no}">삭제</a></td>
 								</tr>
 								<tr><td colspan=4>${fn:replace(vo.contents, newline, "<br>")}</tr>
 							</table>
