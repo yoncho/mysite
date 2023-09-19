541d60ba29255051accd6c68884acacd7cd10041
yoncho <joyonghyeon13@gmail.com>
2023-09-19 11:59:00 +0900

    study - aop in mysite03

--- mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.poscodx.mysite.vo.GuestbookVo;
 
@@ -31,6 +32,8 @@ public class GuestbookRepository {
 	}
 	
 	public List<GuestbookVo> findAll() {
+
 		return sqlSession.selectList("guestbook.findAll");
+		
 	}
 }
