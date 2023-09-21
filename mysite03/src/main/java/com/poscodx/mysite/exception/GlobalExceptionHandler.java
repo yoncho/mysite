33ef03f13884a62f6d1af1aa2f805d349a64ec59
yoncho <joyonghyeon13@gmail.com>
2023-09-21 12:15:46 +0900

    study - lgback/ logger #finish

--- mysite03/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/com/poscodx/mysite/exception/GlobalExceptionHandler.java
@@ -3,6 +3,9 @@ package com.poscodx.mysite.exception;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,13 +13,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	
+	private static final Log logger = LogFactory.getLog(GlobalExceptionHandler.class);
+	
 	@ExceptionHandler(Exception.class)
 	public String handlerException(Model model, Exception e) {
 		//1. Loging
- 		//System.out.println(e);
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
-		System.out.println(errors.toString()); //file로 내보내야함..!!
+		logger.error(errors.toString());
 		
 		//2. 사과 페이지
 		model.addAttribute("errors", errors.toString());
