48d86f4bdd050341330353177c0993ac1fa5a926
yoncho <joyonghyeon13@gmail.com>
2023-09-25 14:05:52 +0900

    study - mysite03

--- mysite03/src/main/java/com/poscodx/mysite/controller/GalleryController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/GalleryController.java
@@ -1,14 +1,26 @@
 package com.poscodx.mysite.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.poscodx.mysite.security.Auth;
+import com.poscodx.mysite.service.GalleryService;
 
 @Controller
 @RequestMapping("/gallery")
 public class GalleryController {
-
+	@Autowired
+	private GalleryService galleryService;
+	
 	@RequestMapping("")
 	public String index() {
 		return "gallery/index";
 	}
+	
+	@Auth(Role="ADMIN")// add/ remove의 경우! 사용해야함..
+	public void add() {
+		
+	}
+	
 }
