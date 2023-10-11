f7004be513002abcb6c6b49c598c702b72e859c0
yoncho <joyonghyeon13@gmail.com>
2023-10-11 19:50:17 +0900

    mysite06

--- mysite06/src/main/java/com/poscodx/mysite/BootInitializer.java
+++ mysite06/src/main/java/com/poscodx/mysite/BootInitializer.java
@@ -4,7 +4,6 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 public class BootInitializer extends SpringBootServletInitializer {
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(MySiteApplication.class);
--- mysite06/src/main/java/com/poscodx/mysite/MySiteApplication.java
+++ mysite06/src/main/java/com/poscodx/mysite/MySiteApplication.java
@@ -7,5 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MySiteApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MySiteApplication.class, args);
- 	}
+	}
 }
